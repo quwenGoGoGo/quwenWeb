e22cb50d59ddb8ffa10ecaf5757b6b383dd1347c
caimengyuan <983067683@qq.com>
2019-01-11 09:48:43 +0800

    category 实体类

--- src/main/java/com/groupeight/quwen/entity/Category.java
+++ src/main/java/com/groupeight/quwen/entity/Category.java
@@ -1,4 +1,39 @@
 package com.groupeight.quwen.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue
+    private Long cateID;
+    private String title;
+    private Integer sort;
+
+    public Long getCateID() {
+        return cateID;
+    }
+
+    public void setCateID(Long cateID) {
+        this.cateID = cateID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
 }
