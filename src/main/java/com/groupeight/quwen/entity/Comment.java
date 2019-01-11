8282a26cf9c8ee0030e18676b89ed78f68558dc2
f1309203474 <1309203474@qq.com>
2019-01-11 10:04:30 +0800

    comment definition

--- src/main/java/com/groupeight/quwen/entity/Comment.java
+++ src/main/java/com/groupeight/quwen/entity/Comment.java
@@ -1,6 +1,14 @@
 package com.groupeight.quwen.entity;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue
     private Integer commentID;
     private String UserID;
     private String commentContent;
