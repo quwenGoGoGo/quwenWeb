f1baeb3fd513967f8179816b4cb39d956a251dc9
f1309203474 <1309203474@qq.com>
2019-01-11 09:53:58 +0800

    comment definition

--- src/main/java/com/groupeight/quwen/entity/Comment.java
+++ src/main/java/com/groupeight/quwen/entity/Comment.java
@@ -1,4 +1,40 @@
 package com.groupeight.quwen.entity;
 
 public class Comment {
+    private Integer commentID;
+    private String UserID;
+    private String commentContent;
+    private String commentDate;
+
+    public Integer getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(Integer commentID) {
+        this.commentID = commentID;
+    }
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
+
+    public String getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(String commentDate) {
+        this.commentDate = commentDate;
+    }
 }
