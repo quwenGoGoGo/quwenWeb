3f8d4f98c6f0b7384b5a9ad8c2180c2c1b4139dd
kekeyouxing <844765548@qq.com>
2019-01-11 11:54:50 +0800

    test

--- src/main/java/com/groupeight/quwen/entity/Collect.java
+++ src/main/java/com/groupeight/quwen/entity/Collect.java
@@ -1,4 +1,38 @@
 package com.groupeight.quwen.entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
 public class Collect {
+
+    @Id
+    @GeneratedValue
+    private Long ID;
+
+    private java.sql.Date time;
+
+    public Collect(Date time) {
+        this.time = time;
+    }
+
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date colTime) {
+        this.time = time;
+    }
 }
+
--- src/main/java/com/groupeight/quwen/entity/User.java
+++ src/main/java/com/groupeight/quwen/entity/User.java
@@ -1,4 +1,63 @@
 package com.groupeight.quwen.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "name is mandatory")
+    private String name;
+
+    @NotBlank(message = "password is mandatory")
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Collect> collect;
+
+    public User() {}
+
+    public User(@NotBlank(message = "name is mandatory") String name,
+                @NotBlank(message = "password is mandatory") String password,
+                List<Collect> collect) {
+        this.name = name;
+        this.password = password;
+        this.collect = collect;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Collect> getCollect() {
+        return collect;
+    }
+
+    public void setCollect(List<Collect> collect) {
+        this.collect = collect;
+    }
 }
