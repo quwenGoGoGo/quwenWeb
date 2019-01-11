8e5a4f66e4eb1438f536984d664843a74fb9c85f
zhangjiao <1156759563@qq.com>
2019-01-11 11:20:54 +0800

    collect

--- src/main/java/com/groupeight/quwen/entity/Collect.java
+++ src/main/java/com/groupeight/quwen/entity/Collect.java
@@ -1,4 +1,50 @@
 package com.groupeight.quwen.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
 public class Collect {
+
+    @Id
+    @GeneratedValue
+    private Long ID;
+    private Long newsId;
+    private Long userId;
+    private java.sql.Date colTime;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public Long getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(Long newsId) {
+        this.newsId = newsId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getColTime() {
+        return colTime;
+    }
+
+    public void setColTime(Date colTime) {
+        this.colTime = colTime;
+    }
 }
+
