77b7429c4a63e7446d77754866d9a11eff1ddf60
f1309203474 <1309203474@qq.com>
2019-01-15 17:25:10 +0800

    edit CommentService and CommentServiceImpl;

--- src/main/java/com/groupeight/quwen/service/CommentService.java
+++ src/main/java/com/groupeight/quwen/service/CommentService.java
@@ -1,4 +1,17 @@
 package com.groupeight.quwen.service;
 
+import com.groupeight.quwen.entity.Comment;
+
+import java.util.List;
+
 public interface CommentService {
+    List<Comment> getAllComment();
+
+    Comment addComment(Comment comment);
+
+    Comment updateComment(Comment comment);
+
+    void deleteComment(Long commentID);
+
+    Comment getCommentByID(Long commentID);
 }
--- src/main/java/com/groupeight/quwen/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/groupeight/quwen/serviceImpl/CommentServiceImpl.java
@@ -1,4 +1,40 @@
 package com.groupeight.quwen.serviceImpl;
 
-public class CommentServiceImpl {
+import com.groupeight.quwen.entity.Comment;
+import com.groupeight.quwen.repository.CommentRepository;
+import com.groupeight.quwen.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public List<Comment> getAllComment() {
+        return commentRepository.findAll();
+    }
+
+    @Override
+    public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public Comment updateComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteComment(Long commentID) {
+        commentRepository.deleteById(commentID);
+    }
+
+    @Override
+    public Comment getCommentByID(Long commentID) {
+        return commentRepository.findById(commentID).get();
+    }
 }
