9bc94df696dda612387ae50281f82fdbdb3cd966
caimengyuan <983067683@qq.com>
2019-01-15 15:47:25 +0800

    Merge branch 'master' of https://github.com/quwenGoGoGo/quwenWeb
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/com/groupeight/quwen/controller/CategoryController.java
+++ src/main/java/com/groupeight/quwen/controller/CategoryController.java
@@ -1,10 +1,28 @@
 package com.groupeight.quwen.controller;
 
+import com.groupeight.quwen.entity.Category;
+import com.groupeight.quwen.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("cate")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
 
+    @GetMapping("index")
+    public String index(){return "index";}
+
+    @GetMapping("toList")
+    public String getCategoryList(Model model){
+        List<Category> category=categoryService.getAllCategories();
+        model.addAttribute("category",category);
+        return "cate_list";
+    }
 }
