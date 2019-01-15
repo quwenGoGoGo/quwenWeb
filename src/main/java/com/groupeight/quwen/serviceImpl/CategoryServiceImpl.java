c96664d103401035652199c6e8688d97b95a5968
Caiminm1n <M201876091@hust.edu.cn>
2019-01-15 15:39:33 +0800

    version1.0

--- src/main/java/com/groupeight/quwen/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/groupeight/quwen/serviceImpl/CategoryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.groupeight.quwen.serviceImpl;
 
+import com.groupeight.quwen.entity.Category;
 import com.groupeight.quwen.repository.CategoryRepository;
 import com.groupeight.quwen.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
-
     @Override
-
-
+    public List<Category> getAllCategory() {
+        return null;
+    }
 }
