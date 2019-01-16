648e7faa71ed4eb995b25cca9e1967eaf1648f25
f1309203474 <1309203474@qq.com>
2019-01-16 15:57:20 +0800

    partial CommentController

--- src/main/java/com/groupeight/quwen/controller/CommentController.java
+++ src/main/java/com/groupeight/quwen/controller/CommentController.java
@@ -1,4 +1,47 @@
 package com.groupeight.quwen.controller;
 
+import com.groupeight.quwen.entity.Comment;
+import com.groupeight.quwen.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("comments")
 public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping("/all")
+    public String getAllCmt(Model model){
+       List<Comment> lists = commentService.getAllComment();
+       model.addAttribute("cmts", lists);
+       return "comment_list";
+    }
+
+    @GetMapping("/add")
+    public String toAdd(Model model){
+        model.addAttribute("cmts", new Comment());
+        return "comment_add";
+    }
+
+    @PostMapping("/add")
+    public  String add(Model model, @ModelAttribute Comment cmts){
+        commentService.addComment(cmts);
+        List<Comment> lists = commentService.getAllComment();
+        model.addAttribute("cmts", lists);
+        return "comment_list";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String toUpdate(Model model, @PathVariable("id") Long commentID){
+        model.addAttribute("cmts", commentService.getCommentByID(commentID) );
+        return "comment_update";
+    }
+
+//    @RequestMapping("/update/{id}")
+//    public String update(Model model, @PathVariable Long commentID, @ModelAttribute Comment cmts){}
 }
